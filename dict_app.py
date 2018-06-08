4d45d7764938cb56a96a3518a9293b701a520517
David Nallapu <davidsam1998@gmail.com>
2018-06-09 02:29:36 +0530

    First commit

---
+++ dict_app.py
@@ -0,0 +1,20 @@
+import json
+from difflib import get_close_matches
+data =json.load(open("data.json"))
+global new_word
+def translate(w):
+	if w in data:
+		return data[word]
+	elif len(get_close_matches(w,data.keys()))>0:
+		yn =input("Did you mean %s(y/n)?" % get_close_matches(w,data.keys())[0])
+		if yn =="y":
+			return data[get_close_matches(w,data.keys())[0]]
+		else:
+			exit(0)
+		
+	else:
+		return "Word doesn't exist.Check it"
+
+word = input("Enter word:").lower()
+
+print(translate(word))
